aec33bd8f50bacf2f2c301fb24363d50c0848b35
koxrel <15571888+koxrel@users.noreply.github.com>
2020-10-28 21:21:43 +0300

    bootstrap the new project

---
+++ src/main/java/me/koxrel/accessticketstracker/AccessTicketsTrackerApplication.java
@@ -0,0 +1,13 @@
+package me.koxrel.accessticketstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessTicketsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessTicketsTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/koxrel/accessticketstracker/AccessTicketsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.koxrel.accessticketstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessTicketsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
